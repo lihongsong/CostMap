97b43a0815b397cbeab82b688be2be7009ae19aa
zhangyz <zhangyz@2345.com>
2018-06-29 15:02:27 +0800

    app delegate 修改， git ignore 添加

--- HuaQianWuYou/AppDelegate.m
+++ HuaQianWuYou/AppDelegate.m
@@ -12,9 +12,13 @@
 #import "UtilitiesDefine.h"
 #import "DebugChangeHostVC.h"
 #import "ThirdPartKeyDefine.h"
+#import <RCMobClick/RCMobClick.h>
+#import <UMMobClick/MobClick.h>
 #import "DeviceHelp.h"
 #import "IntroduceViewController.h"
 #import "TouchIDViewController.h"
+#import "LaunchViewController.h"
+#import "AppDelegate+SDKRegister.h"
 // iOS10注册APNs所需头文件
 #ifdef NSFoundationVersionNumber_iOS_9_x_Max
 #import <UserNotifications/UserNotifications.h>
@@ -29,99 +33,87 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    WeakObj(self);
-    /** Window设置 */
-    [self setUpWindow];
+    //移动武林榜
+    [RCMobClick startWithAppkey:MobClick_AppKey projectName:MobClick_ProjectName channelId:MobClick_Channel isIntegral:YES];
     
-//    /** 通过通知栏调起APP处理通知信息 */
+    //友盟  老的58fec71d677baa3921000b81
+    UMConfigInstance.appKey = UMen_AppKey;
+    UMConfigInstance.channelId = UMen_channelId;
+    NSString *appVersion = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+    [MobClick setAppVersion:appVersion];
+    [MobClick startWithConfigure:UMConfigInstance];
+    
+#ifdef DEBUG
+    [MobClick setLogEnabled:YES];//调试用，发布需改回默认no
+#endif
+    
+    WeakObj(self);
+    [self registerAppUpdate];
+    //    /** 通过通知栏调起APP处理通知信息 */
     NSDictionary *remoteNotification = [launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
-//
-//    /** 启动图设置 */
-    [self setupLaunchViewControllerWithRemoteNotification:remoteNotification];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    LaunchViewController *launchVC = [LaunchViewController new];
+    self.window.rootViewController = launchVC;
+    [self.window makeKeyAndVisible];
+    launchVC.accomplishBlock = ^(NSString *exampleCreditScore) {
+        StrongObj(self);
+        if (![exampleCreditScore isEqualToString:@"88"]) {
+            [self setupLaunchViewControllerWithRemoteNotification:remoteNotification];
+            [self checkUpdate];
+        }else{
+            [self setUpViewControllerWithHighScoreWithRemoteNotificaton:remoteNotification launchOptions:launchOptions];
+            [self checkUpdate];
+        }
+    };
     
     //开始检测网络状态(主要针对IOS10以后的网络需要授权问题)
     //如果还没有网络授权,则在用户选择后再发送一些API
     [[AFNetworkReachabilityManager sharedManager] startMonitoring];
     [[AFNetworkReachabilityManager sharedManager] setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
         if (status == AFNetworkReachabilityStatusReachableViaWWAN || status == AFNetworkReachabilityStatusReachableViaWiFi) {
-            [selfWeak appBasicAPISend:launchOptions WithBlock:YES];
+            //[selfWeak appBasicAPISend:launchOptions WithBlock:YES];
         }
     }];
     return YES;
 }
 
-
-#pragma mark window 设置
--(void)setUpWindow{
-    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
-}
-
-#pragma mark - 启动图设置
-- (void)setupLaunchViewControllerWithRemoteNotification:(NSDictionary *)remoteNotification {
-//#ifdef DEBUG
-//    //等设计稿
-////    if ([DeviceHelp isAppFirstInstall]) {
-////        IntroduceViewController *introduceVC = [[IntroduceViewController alloc]init];
-////         self.window.rootViewController = introduceVC;
-////    }else{
-//        DebugChangeHostVC *debugVC = [[DebugChangeHostVC alloc]init];
-//        self.window.rootViewController = debugVC;
-//        debugVC.rootStartVC = ^{
-//            MainTabBarViewController *rootViewController = [[MainTabBarViewController alloc]init];
-//            [self restoreRootViewController:rootViewController];
-//        };
-//  //  }
-//#else
-//    MainTabBarViewController *rootViewController = [[MainTabBarViewController alloc]init];
-//    self.window.rootViewController = rootViewController;
-//    UIWindow *oldWindow = self.window;
-//    // 不确定用不用自定义sdk
-//    __block HJGuidePageWindow *guideWindow = [HJGuidePageWindow sheareGuidePageWindow:GuidePageAPPLaunchStateNormal];
-//    WeakObj(self)
-//    HJGuidePageViewController *launchVC = [guideWindow makeHJGuidePageWindow:^(HJGuidePageViewController *make) {
-//        StrongObj(self)
-//        // 1秒 网络加载  3秒图片加载
-//        make.setTimer(0, 5, nil, YES);
-//        make.setAnimateFinishedBlock(^(id info) {
-//
-//            self.window = oldWindow;
-//            [self restoreRootViewController:rootViewController];
-//            [self.window makeKeyWindow];
-//
-//            guideWindow.hidden = YES;
-//            [guideWindow removeFromSuperview];
-//
-//        });
-//        make.setCountdownBtnBlock(^(UIButton *btn) {
-//            btn.frame = CGRectMake((SWidth - 66 - 30), (NavigationHeight + 10), 66, 28);
-//        });
-//    }];
-//    self.window.rootViewController = launchVC;
-//    [HJGuidePageWindow show];
-//
-//    //    JKLaunchManager *launchManager = [[JKLaunchManager alloc] init];
-//    //    launchManager.guideVC = launchVC;
-//    //    [launchManager showLanuchPageModel];
-//
-//#endif
-
+- (void)setUpViewControllerWithHighScoreWithRemoteNotificaton:(NSDictionary *)remoteNotification launchOptions:(NSDictionary *)launchOptions {
+    //H5 没有H5，先用原生替代
     NSUserDefaults *userDefault = [NSUserDefaults standardUserDefaults];
     
     if (![userDefault boolForKey:@"kCachedTouchIdStatus"]) {
         MainTabBarViewController *rootViewController = [[MainTabBarViewController alloc]init];
-        self.window.rootViewController = rootViewController;
+        [self restoreRootViewController:rootViewController];
     } else {
         TouchIDViewController *touchIDVC = [[TouchIDViewController alloc]init];
         self.window.rootViewController = touchIDVC;
         touchIDVC.rootStartVC = ^{
             MainTabBarViewController *rootViewController = [[MainTabBarViewController alloc]init];
-            self.window.rootViewController = rootViewController;
-            // [self restoreRootViewController:rootViewController];
+            [self restoreRootViewController:rootViewController];
         };
     }
+    //注册下apns
+}
+
+#pragma mark - 启动图设置
+- (void)setupLaunchViewControllerWithRemoteNotification:(NSDictionary *)remoteNotification {
+    NSUserDefaults *userDefault = [NSUserDefaults standardUserDefaults];
     
+    if (![userDefault boolForKey:@"kCachedTouchIdStatus"]) {
+        MainTabBarViewController *rootViewController = [[MainTabBarViewController alloc]init];
+        [self restoreRootViewController:rootViewController];
+    } else {
+        TouchIDViewController *touchIDVC = [[TouchIDViewController alloc]init];
+        self.window.rootViewController = touchIDVC;
+        touchIDVC.rootStartVC = ^{
+            MainTabBarViewController *rootViewController = [[MainTabBarViewController alloc]init];
+            [self restoreRootViewController:rootViewController];
+        };
+    }
+    //注册下apns
+   
 }
 
 #pragma mark - 渐变动画更换RootVC
@@ -138,108 +130,6 @@
                     }
                     completion:nil];
 }
-
-//一些基本API调用
-- (void)appBasicAPISend:(NSDictionary *)launchOptions WithBlock:(BOOL)isBlock {
-    static BOOL isSend = NO;
-    //确保APP启动后只发送一次
-    if (!isSend) {
-        if (isBlock) {
-            //发送过后就断开网络监控
-            [[AFNetworkReachabilityManager sharedManager] stopMonitoring];
-        }
-        isSend = YES;
-        
-        // 启动APNs.
-        //[self registerJPush:launchOptions];
-        
-        // TalkingData 统计SDK初始化
-//        [TalkingData sessionStarted:TalkingData_AppId withChannelId:AppChannel];
-//        [TalkingDataAppCpa init:TalkingDataAppCpa_AppId withChannelId:AppChannel];
-        
-//        //bugly注册
-//        BuglyConfig *buglyConfig = [[BuglyConfig alloc]init];
-//        buglyConfig.blockMonitorEnable = YES;
-//
-//#if defined (ReleaseNet)
-//        [Bugly startWithAppId:Bugly_AppId config:buglyConfig];
-//#elif defined (PreNet)
-//        [Bugly startWithAppId:Bugly_AppIdDebug config:buglyConfig];
-//#else
-//        [Bugly startWithAppId:Bugly_AppIdDebug developmentDevice:YES config:buglyConfig];
-//#endif
-        
-    }
-}
-
-/*
-- (void)registerJPush:(NSDictionary *)launchOptions{
-    //Required
-    //notice: 3.0.0及以后版本注册可以这样写，也可以继续用之前的注册方式
-    JPUSHRegisterEntity * entity = [[JPUSHRegisterEntity alloc] init];
-    entity.types = JPAuthorizationOptionAlert|JPAuthorizationOptionBadge|JPAuthorizationOptionSound;
-    if ([[UIDevice currentDevice].systemVersion floatValue] >= 8.0) {
-        // 可以添加自定义categories
-        // NSSet<UNNotificationCategory *> *categories for iOS10 or later
-        // NSSet<UIUserNotificationCategory *> *categories for iOS8 and iOS9
-    }
-    [JPUSHService registerForRemoteNotificationConfig:entity delegate:self];
-
-    // Required
-    // init Push
-    [JPUSHService setupWithOption:launchOptions appKey:JPush_AppKey
-                          channel:JPush_Channel
-                 apsForProduction:isProduction
-            advertisingIdentifier:nil];
-}
-
-- (void)application:(UIApplication *)application
-didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-    // 注册APNs成功并上报DeviceToken
-    /// Required - 注册 DeviceToken
-    [JPUSHService registerDeviceToken:deviceToken];
-}
-
-- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
-    //Optional。实现注册APNs失败接口（可选）
-    NSLog(@"did Fail To Register For Remote Notifications With Error: %@", error);
-}
-
-#pragma mark- JPUSHRegisterDelegate
-
-// iOS 10 Support
-- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(NSInteger))completionHandler {
-    // Required
-    NSDictionary * userInfo = notification.request.content.userInfo;
-    if([notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
-        [JPUSHService handleRemoteNotification:userInfo];
-    }
-    completionHandler(UNNotificationPresentationOptionAlert); // 需要执行这个方法，选择是否提醒用户，有Badge、Sound、Alert三种类型可以选择设置
-}
-
-// iOS 10 Support
-- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)())completionHandler {
-    // Required
-    NSDictionary * userInfo = response.notification.request.content.userInfo;
-    if([response.notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
-        [JPUSHService handleRemoteNotification:userInfo];
-    }
-    completionHandler();  // 系统要求执行这个方法
-}
-
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
-    
-    // Required, iOS 7 Support
-    [JPUSHService handleRemoteNotification:userInfo];
-    completionHandler(UIBackgroundFetchResultNewData);
-}
-
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
-    
-    // Required,For systems with less than or equal to iOS6
-    [JPUSHService handleRemoteNotification:userInfo];
-}
- */
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
