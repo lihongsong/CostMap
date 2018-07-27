5c7ec1bdffa346ab1502579522c60f0ff54f6d14
zhangyz <zhangyz@2345.com>
2018-07-27 16:52:05 +0800

    三方页添加精准推荐，三方跳appstore优化，添加代码混淆方法

--- HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
+++ HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
@@ -1,7 +1,7 @@
 #ifndef funcDefine
 #define funcDefine_h
-//confuse string at Mon Jun 25 19:49:36 CST 2018
-#define jsBundleURLForBundleRoot ckUCBLEhzGcxEXRO
-#define RCTBundleURLProvider NiaJOByHrXdqyNZZ
-#define RCTRootView jEirrituBZHyYNkB
+//confuse string at Fri Jul 27 16:47:18 CST 2018
+#define setupLaunchViewControllerWithRemoteNotification gykyWFQDMCkLNBrS
+#define setUpViewControllerWithHighScoreWithRemoteNotificaton thzvXzKPkCUDIwbc
+#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 XbaOmszxYpYBLNtF
 #endif
