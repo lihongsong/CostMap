14fe7e3f1cdfb42c239754d3a0ac41d11b5a7ca8
zhangyz <zhangyz@2345.com>
2018-07-31 16:15:48 +0800

    H5调原生toast优化

--- HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
+++ HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
@@ -1,7 +1,7 @@
 #ifndef funcDefine
 #define funcDefine_h
-//confuse string at Tue Jul 31 16:07:45 CST 2018
-#define setupLaunchViewControllerWithRemoteNotification OxhZJAMQIjPDRAuz
-#define setUpViewControllerWithHighScoreWithRemoteNotificaton hgCjFJrPJCTzRPRJ
-#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 TJbOnZgfTcHSWrQf
+//confuse string at Tue Jul 31 16:13:49 CST 2018
+#define setupLaunchViewControllerWithRemoteNotification fSXKbNDlChHNjnoc
+#define setUpViewControllerWithHighScoreWithRemoteNotificaton MDvuSYzYXOINDEii
+#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 AkyJSqWzAtoXtfjT
 #endif
