e49f50bcdf7fb97c7c44413f25e314fb8aa361fd
zhangyz <zhangyz@2345.com>
2018-08-01 00:10:19 +0800

    去除git写成文件代码

--- HuaQianWuYou/Active/ActivePage/Controller/ActiveViewController.m
+++ HuaQianWuYou/Active/ActivePage/Controller/ActiveViewController.m
@@ -88,7 +88,6 @@ static NSString * const kJSSetUpName = @"javascriptSetUp.js";
     [notificatoinCenter addObserver:self selector:@selector(appWillEnterForeground:) name:@"kAppWillEnterForeground" object:nil];
     
     [notificatoinCenter addObserver:self selector:@selector(topPreRecommend:) name:@"kAppClickTopPreRecommend" object:nil];
-   // [[HQWYJavaScriptSourceHandler new] didReceiveMessage:nil hander:nil];
 }
 
 - (instancetype)init{
