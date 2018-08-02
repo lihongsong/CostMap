5aef3590e504677e6b364ef5afe9c911ab513cd5
zhangyz <zhangyz@2345.com>
2018-08-02 09:39:27 +0800

    花钱无忧征信v2.0

--- HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
+++ HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
@@ -1,7 +1,7 @@
 #ifndef funcDefine
 #define funcDefine_h
-//confuse string at Wed Aug  1 00:12:06 CST 2018
-#define setupLaunchViewControllerWithRemoteNotification ekZvqSGTfvvaIeNY
-#define setUpViewControllerWithHighScoreWithRemoteNotificaton NWRIwQXUBEqtyHOh
-#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 AaqkTRcRmdmrekuD
+//confuse string at Thu Aug  2 09:38:03 CST 2018
+#define setupLaunchViewControllerWithRemoteNotification OVXXMzILqtuIJstg
+#define setUpViewControllerWithHighScoreWithRemoteNotificaton bxIkcHpjjosKGdhN
+#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 yYOFnOrHGfQpoOMs
 #endif
