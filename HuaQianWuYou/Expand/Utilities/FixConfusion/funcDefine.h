66adde3cd2c49e89396f669644d675d44a1385f4
zhangyz <zhangyz@2345.com>
2018-08-03 18:05:47 +0800

    merge webprogress 进度条分支

--- HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
+++ HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
@@ -1,7 +1,7 @@
 #ifndef funcDefine
 #define funcDefine_h
-//confuse string at Fri Aug  3 14:04:40 CST 2018
-#define setupLaunchViewControllerWithRemoteNotification JFtonWZZMEkMsbYD
-#define setUpViewControllerWithHighScoreWithRemoteNotificaton ztylQEcTXbpXebUe
-#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 gtTionxNSnRMRuHR
+//confuse string at Fri Aug  3 18:05:00 CST 2018
+#define setupLaunchViewControllerWithRemoteNotification YeAgHvEPtFRBAHqi
+#define setUpViewControllerWithHighScoreWithRemoteNotificaton jhgOAZYMeTABMrbx
+#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 BGzFpxHQSKQhzAio
 #endif
