d5628dd4c2a018bdab2668d3e219b31894220cd2
zhangyz <zhangyz@2345.com>
2018-08-03 14:08:51 +0800

    gitignore修改

--- HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
+++ HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
@@ -1,7 +1,7 @@
 #ifndef funcDefine
 #define funcDefine_h
-//confuse string at Fri Aug  3 13:48:14 CST 2018
-#define setupLaunchViewControllerWithRemoteNotification NXRdhRhoEwszRRZI
-#define setUpViewControllerWithHighScoreWithRemoteNotificaton atIgWcbBmNEZryFY
-#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 BmhCMVxlJWKteSMo
+//confuse string at Fri Aug  3 14:04:40 CST 2018
+#define setupLaunchViewControllerWithRemoteNotification JFtonWZZMEkMsbYD
+#define setUpViewControllerWithHighScoreWithRemoteNotificaton ztylQEcTXbpXebUe
+#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 gtTionxNSnRMRuHR
 #endif
