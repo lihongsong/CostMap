3c27e2d000a25923c47e05917eb1c903357f72e2
zhangyz <zhangyz@2345.com>
2018-08-06 11:31:34 +0800

    优化进度条，采用自定义进度条

--- HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
+++ HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
@@ -1,7 +1,7 @@
 #ifndef funcDefine
 #define funcDefine_h
-//confuse string at Fri Aug  3 14:04:40 CST 2018
-#define setupLaunchViewControllerWithRemoteNotification JFtonWZZMEkMsbYD
-#define setUpViewControllerWithHighScoreWithRemoteNotificaton ztylQEcTXbpXebUe
-#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 gtTionxNSnRMRuHR
+//confuse string at Mon Aug  6 11:29:16 CST 2018
+#define setupLaunchViewControllerWithRemoteNotification uCYfbePgyOgREXog
+#define setUpViewControllerWithHighScoreWithRemoteNotificaton jbKaKMMNKbaseVUa
+#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 epPRQloqMPHWVuuf
 #endif
