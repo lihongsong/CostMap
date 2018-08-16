fc728e52fd6609e9e771e656ac2bac7334009a70
zhangyz <zhangyz@2345.com>
2018-08-16 15:48:19 +0800

    merge thirdwebduration

--- HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
+++ HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
@@ -1,7 +1,7 @@
 #ifndef funcDefine
 #define funcDefine_h
-//confuse string at Thu Aug 16 15:40:02 CST 2018
-#define setupLaunchViewControllerWithRemoteNotification MzSYCzlumXCGKcBZ
-#define setUpViewControllerWithHighScoreWithRemoteNotificaton wWHMaUrbszmtVtIV
-#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 VkMyYJRglLveunnc
+//confuse string at Thu Aug 16 15:47:51 CST 2018
+#define setupLaunchViewControllerWithRemoteNotification VlwHdwRCxwOJdysv
+#define setUpViewControllerWithHighScoreWithRemoteNotificaton DULLrNojrrVCddKA
+#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 BwXROMSLtRWOqgum
 #endif
