3ccab2bcf083f861e870882f981aba6244309621
zhangyz <zhangyz@2345.com>
2018-08-16 17:14:38 +0800

    backgorund mode关闭，host替换成t1

--- HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
+++ HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
@@ -1,7 +1,7 @@
 #ifndef funcDefine
 #define funcDefine_h
-//confuse string at Thu Aug 16 15:53:53 CST 2018
-#define setupLaunchViewControllerWithRemoteNotification adHRTAEbzrxPchQZ
-#define setUpViewControllerWithHighScoreWithRemoteNotificaton ufosgdifCeEoTnau
-#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 raQCLQLlhxjSnALg
+//confuse string at Thu Aug 16 17:12:18 CST 2018
+#define setupLaunchViewControllerWithRemoteNotification gsoPTIxFoHfaNlvw
+#define setUpViewControllerWithHighScoreWithRemoteNotificaton iGdKMFMyJIrtncSk
+#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 ddrKLhaHVWoyIGfF
 #endif
