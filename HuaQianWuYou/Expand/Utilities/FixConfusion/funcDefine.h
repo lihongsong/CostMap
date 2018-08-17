71e91086275822239951b1b19b87edf895a141a3
zhangyz <zhangyz@2345.com>
2018-08-17 09:04:13 +0800

    jekins打包失败查看

--- HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
+++ HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
@@ -1,7 +1,7 @@
 #ifndef funcDefine
 #define funcDefine_h
-//confuse string at Thu Aug 16 17:16:30 CST 2018
-#define setupLaunchViewControllerWithRemoteNotification nJsXsJfpsYMqwGvS
-#define setUpViewControllerWithHighScoreWithRemoteNotificaton XAyEdXjHHxXUSKVb
-#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 aVQVhuYQKdEKFMdD
+//confuse string at Fri Aug 17 08:57:40 CST 2018
+#define setupLaunchViewControllerWithRemoteNotification lVVWLwrpPTLzFtjt
+#define setUpViewControllerWithHighScoreWithRemoteNotificaton JSeZYXjBTauNtWgu
+#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 UXaoMFsQpXXVudMm
 #endif
