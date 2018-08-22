548d2e93252eaf95b9a756e237228307db4dbd62
zhangyz <zhangyz@2345.com>
2018-08-22 16:51:34 +0800

    v2.1打包testflight版本

--- HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
+++ HuaQianWuYou/Expand/Utilities/FixConfusion/funcDefine.h
@@ -1,7 +1,7 @@
 #ifndef funcDefine
 #define funcDefine_h
-//confuse string at Tue Aug 21 14:59:20 CST 2018
-#define setupLaunchViewControllerWithRemoteNotification KcbXoPDllMuVAEGa
-#define setUpViewControllerWithHighScoreWithRemoteNotificaton fWaGjksYySKibfyp
-#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 gGlHvwJvtNPCcKFD
+//confuse string at Wed Aug 22 16:50:49 CST 2018
+#define setupLaunchViewControllerWithRemoteNotification fInKKXEialmYNlPq
+#define setUpViewControllerWithHighScoreWithRemoteNotificaton wfyUzBcnxGQQIAEd
+#define 您的位置将被用来精准匹配贷款产品，并享受贷款优惠服务 gnRypmWnWpsQjfMc
 #endif
